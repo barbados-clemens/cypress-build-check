ec44945ec310c557e7bafd3f578d8f89b83c5c2e
Caleb Ukle <caleb@nrwl.io>
2025-04-15 09:21:10 -0500

    sync

--- apps/demo-e2e/cypress.config.ts
+++ apps/demo-e2e/cypress.config.ts
@@ -4,15 +4,18 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     ...nxE2EPreset(__filename, {
-      cypressDir: 'src',
+      cypressDir: 'src/e2e',
       webServerCommands: {
         default: 'npx nx run demo:serve',
-        production: 'npx nx run demo:serve-static',
+        production: 'npx nx run demo:serve:production',
       },
       ciWebServerCommand: 'npx nx run demo:serve-static',
       ciBaseUrl: 'http://localhost:4200',
     }),
     baseUrl: 'http://localhost:4200',
+    specPattern: 'src/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'src/support/e2e.ts',
+    fixturesFolder: 'src/fixtures',
     video: true,
     screenshotOn: true,
   },
